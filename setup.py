ec3d1f43d447ff57e8d218cccd4e3d9ffd7f7430
Jamie Norrish <jamie@artefact.org.nz>
2015-03-17 14:07:12 +1300

    Tightened package data glob, to avoid picking up ~ files.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(name='ddh_django_utils',
-      version = '0.9',
+      version = '0.9.2',
       description = 'Reusable Django app containing utilities for DDH projects',
       url = 'https://github.com/kcl-ddh/ddh_django_utils',
       author = 'Jamie Norrish',
       author_email = 'jamie@artefact.org.nz',
       packages = find_packages(),
       package_data = {
-          'ddh_utils': ['templates/includes/*']
+          'ddh_utils': ['templates/includes/*.html']
       },
       classifiers = [
           'Framework :: Django',
